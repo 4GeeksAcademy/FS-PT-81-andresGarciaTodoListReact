4835d0a8a73ec09fa034a855e6d8f584a85258b0
Elandruf <andreseduardo2710@gmail.com>
2024-11-13 15:46:35 +0000

    Todo List

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,42 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
-const Home = () => {
+function Home() {
+	const [work, setWork] = useState('');
+	const [tareas, setTareas] = useState([]);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+	};
+
+	const handlechange = e => setWork(e.target.value);
+
+	const handleKeyDown = (e) => {
+		if (e.key === "Enter") {
+			setTareas([...tareas, work]);
+			setWork("");
+		}
+	};
+
+	const handleDelete = (index) => {
+		const filtroTareas = tareas.filter((_, indice) => index !== indice);
+		setTareas(filtroTareas);
+	};
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="fondo">
+			<div className="container">
+				<div className="text-center">
+					<h1>TODO</h1>
+					<input type="text" onChange={handlechange} onKeyDown={(e) => handleKeyDown(e)} value={work} />
+					<ul>
+						{tareas.map((item, index) => (<div className="items" key={index}><li>{item}</li><button onClick={() => { handleDelete(index); }}>X</button></div>))}
+					</ul>
+					<p className="item">{tareas.length} {tareas.length === 1 ? "item" : "items"} </p>
+				</div>
+			</div>
 		</div>
 	);
-};
+}
 
 export default Home;
